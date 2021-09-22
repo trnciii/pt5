aa2b4f21975e820d60af0304ce5cd2f6af0be0a9
trnciii <doityan223@gmail.com>
2021-09-22 09:24:25 +0900

    shorten image writer

--- libpt5/include/util.hpp
+++ libpt5/include/util.hpp
@@ -10,22 +10,18 @@
 
 namespace pt5{
 
-void writeImage(const std::string& filename, int w, int h, const std::vector<float>& data){
-	std::vector<uint32_t> pixelsi(w*h);
-	for(int y=0; y<h; y++){
-		for(int x=0; x<w; x++){
-			const int i = w*y + x;
+void writeImage(const std::string& filename, int w, int h, const std::vector<float>& color_float){
+	std::vector<uint32_t> color(w*h);
+	for(int i=0; i<w*h; i++){
+		const uint32_t r(255.99 * std::max(0.0f, std::min(1.0f, color_float[4*i  ])) );
+		const uint32_t g(255.99 * std::max(0.0f, std::min(1.0f, color_float[4*i+1])) );
+		const uint32_t b(255.99 * std::max(0.0f, std::min(1.0f, color_float[4*i+2])) );
+		const uint32_t a(255.99 * std::max(0.0f, std::min(1.0f, color_float[4*i+3])) );
 
-			const uint32_t r(255.99 * std::max(0.0f, std::min(1.0f, data[4*i  ])) );
-			const uint32_t g(255.99 * std::max(0.0f, std::min(1.0f, data[4*i+1])) );
-			const uint32_t b(255.99 * std::max(0.0f, std::min(1.0f, data[4*i+2])) );
-			const uint32_t a(255.99 * std::max(0.0f, std::min(1.0f, data[4*i+3])) );
-
-			pixelsi[i] = (r<<0) | (g<<8) | (b<<16) | (a<<24);
-		}
+		color[i] = (r<<0) | (g<<8) | (b<<16) | (a<<24);
 	}
 
-	stbi_write_png(filename.c_str(), w, h, 4, pixelsi.data(), w*sizeof(uint32_t));
+	stbi_write_png(filename.c_str(), w, h, 4, color.data(), w*sizeof(uint32_t));
 }
 
 } // pt5 namespace
