dcff24204671802a25d34af74b5027c00d185986
trnciii <doityan223@gmail.com>
2021-09-25 19:15:19 +0900

    update interface

--- interface/interface.cpp
+++ interface/interface.cpp
@@ -4,17 +4,73 @@
 #include "pt5.hpp"
 
 
+void createScene(pt5::Scene& scene){
+	pt5::Camera& camera = scene.camera;
+	camera.position = {0, -40, 1};
+	camera.toWorld[0] = {1, 0, 0};
+	camera.toWorld[1] = {0, 0,-1};
+	camera.toWorld[2] = {0, 1, 0};
+	camera.focalLength = 2;
+
+
+	scene.background = make_float3(0.5, 0.2, 0.8);
+
+
+	std::vector<pt5::Vertex> v0 = {
+		{{-4, 0, 6}, {0, -1, 0}},
+		{{-4, 0, 2}, {0, -1, 0}},
+		{{ 0, 0, 2}, {0, -1, 0}},
+		{{ 0, 0, 6}, {0, -1, 0}},
+		{{ 4, 0, 6}, {0, -1, 0}},
+		{{ 4, 0, 2}, {0, -1, 0}}
+	};
+
+	std::vector<pt5::Vertex> v1 = {
+		{{-4, 0, 6-6}, {0, -1, 0}},
+		{{-4, 0, 2-6}, {0, -1, 0}},
+		{{ 0, 0, 2-6}, {0, -1, 0}},
+		{{ 0, 0, 6-6}, {0, -1, 0}},
+		{{ 4, 0, 6-6}, {0, -1, 0}},
+		{{ 4, 0, 2-6}, {0, -1, 0}}
+	};
+
+
+	std::vector<pt5::Face> f0 = {
+		{{0, 1, 2}},
+		{{2, 3, 0}},
+		{{3, 2, 5}},
+		{{5, 4, 3}}
+	};
+
+
+	std::vector<pt5::Face> f1 = {
+		{{0, 1, 2}},
+		{{2, 3, 0}},
+		{{3, 2, 5}},
+		{{5, 4, 3}}
+	};
+
+	scene.meshes.push_back(pt5::TriangleMesh{v0, f0});
+	scene.meshes.push_back(pt5::TriangleMesh{v1, f1});
+}
+
+
 namespace py = pybind11;
 
 PYBIND11_MODULE(core, m) {
-    using namespace pt5;
+	using namespace pt5;
 
-    py::class_<PathTracerState>(m, "PathTracer")
-        .def(py::init<>())
-        .def("init", &PathTracerState::init)
-        .def("setScene", &PathTracerState::setScene)
-        .def("initLaunchParams", &PathTracerState::initLaunchParams)
-        .def("render", &PathTracerState::render)
-        .def("pixels", &PathTracerState::pixels);
+	py::class_<PathTracerState>(m, "PathTracer")
+		.def(py::init<>())
+		.def("init", &PathTracerState::init)
+		.def("setScene", &PathTracerState::setScene)
+		.def("initLaunchParams", &PathTracerState::initLaunchParams)
+		.def("render", &PathTracerState::render)
+		.def("pixels", &PathTracerState::pixels);
+
+
+	py::class_<Scene>(m, "Scene")
+		.def(py::init<>())
+		.def("createDefault", &createScene);
 
 }
--- pt5/test.py
+++ pt5/test.py
@@ -1,11 +1,23 @@
 import core
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 w, h = 1200, 800
 
+scene = core.Scene()
+scene.createDefault()
+
 pt = core.PathTracer()
-pt.buildSBT()
+pt.init()
+pt.setScene(scene)
 pt.initLaunchParams(w, h)
 
 pt.render()
 
-core.writeImage('out_py.png', w, h, pt.pixels())
+pixels4 = np.array(pt.pixels()).reshape((h,w,4))
+
+plt.imshow(pixels4)
+plt.show()
+
+plt.imsave('out_py.png', pixels4)
