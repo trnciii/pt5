5620abff3ee80790345fc61cc9844505b88ba968
trnciii <doityan223@gmail.com>
2021-10-09 17:16:10 +0900

    fix python test

--- interface/testCore.py
+++ interface/testCore.py
@@ -14,31 +14,32 @@ def createScene(scene):
 													[0, 1, 0]]
 
 	# background
-	scene.background = [0.2, 0.1, 0.4]
+	scene.background = [0.8, 0.8, 0.8]
 
 	# material
 	materials = [core.Material() for i in range(3)]
-	materials[0].color = [0.8, 0.8, 0.3]
-	materials[1].color = [0.1, 0.8, 0.8]
-	materials[2].color = [0.8, 0.5, 0.1]
+	materials[0].albedo = [0.8, 0.8, 0.3]
+	materials[1].albedo = [0.1, 0.8, 0.8]
+	materials[2].albedo = [0.8, 0.5, 0.1]
 
 	m2 = core.Material()
-	m2.color = [0.8, 0.3, 0.8]
+	m2.albedo = [0.8, 0.3, 0.8]
 	materials.append(m2)
 
 	scene.materials = materials
 
-	# mesh 0
-	verts0 = [
+	verts0 = np.array([
 		[-4, 0, 6],
 		[-4, 0, 2],
 		[ 0, 0, 2],
 		[ 0, 0, 6],
 		[ 4, 0, 6],
 		[ 4, 0, 2]
-	]
+	])
 
-	normals0 = [
+	verts1 = verts0 - [0,0,6]
+
+	normals = [
 		[0, -1, 0],
 		[0, -1, 0],
 		[0, -1, 0],
@@ -47,7 +48,7 @@ def createScene(scene):
 		[0, -1, 0]
 	]
 
-	indices0 = [
+	indices = [
 		[0, 1, 2],
 		[2, 3, 0],
 		[3, 2, 5],
@@ -57,39 +58,14 @@ def createScene(scene):
 	mIDs0 = [1, 1,	0, 0]
 	mSlots0 = [0, 3]
 
-	# mesh 1
-	verts1 = [
-		[-4, 0, 6-6],
-		[-4, 0, 2-6],
-		[ 0, 0, 2-6],
-		[ 0, 0, 6-6],
-		[ 4, 0, 6-6],
-		[ 4, 0, 2-6]
-	]
-
-	normals1 = [
-		[0, -1, 0],
-		[0, -1, 0],
-		[0, -1, 0],
-		[0, -1, 0],
-		[0, -1, 0],
-		[0, -1, 0]
-	]
-
-	indices1 = [
-		[0, 1, 2],
-		[2, 3, 0],
-		[3, 2, 5],
-		[5, 4, 3]
-	]
 
 	mIDs1 = [1, 2, 0, 3]
 	mSlots1 = [0,1,2,3]
 
 
 	meshes = [
-		core.createTriangleMesh(verts0, normals0, indices0, mIDs0, mSlots0),
-		core.createTriangleMesh(verts1, normals1, indices1, mIDs1, mSlots1)
+		core.createTriangleMesh(verts0, normals, indices, mIDs0, mSlots0),
+		core.createTriangleMesh(verts1, normals, indices, mIDs1, mSlots1)
 	]
 
 	scene.meshes = meshes
@@ -107,7 +83,7 @@ print(scene.camera.toWorld)
 pt = core.PathTracer()
 pt.init()
 pt.setScene(scene)
-pt.initLaunchParams(w, h)
+pt.initLaunchParams(w, h, 1000)
 
 pt.render()
 
