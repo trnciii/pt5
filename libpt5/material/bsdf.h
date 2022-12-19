3c987591d6325781f3042999c6466808cfa08df0
trnciii <doityan223@gmail.com>
2022-12-19 21:20:01 +0900

    define bsdf functions in a different file

---
+++ libpt5/material/bsdf.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../vector_math.h"
+
+namespace pt5{ namespace material{
+
+__device__ float3 sample_cosine_hemisphere(float2 u){
+	u.y *= 2*M_PI;
+	float r = sqrt(u.x);
+	float z = sqrt(1-u.x);
+	return make_float3(r*cos(u.y), r*sin(u.y), z);
+}
+
+__device__ float fresnel(float ni, float eta){
+	const float g2 = eta*eta - 1 + ni;
+	if(g2 < 0) return 1;
+
+	const float g = sqrt(g2);
+	const float add = g + ni;
+	const float sub = g - ni;
+
+	const float frac = (ni*(add)-1)/(ni*sub+1);
+
+	return 0.5 * ((sub*sub)/(add*add)) * (1 + frac*frac);
+}
+
+__device__ float beckmann_g1(float vn, float alpha){
+	const float a = 1/(alpha * sqrt(1/(vn*vn) - 1));
+	return (a<1.6)?
+		(3.535*a + 2.181*a*a) / (1 + 2.276*a + 2.577*a*a)
+		: 1;
+}
+
+}}
--- libpt5/material/material.cu
+++ libpt5/material/material.cu
@@ -3,37 +3,10 @@
 #include "../vector_math.h"
 #include "../kernel/intersection.cuh"
 #include "../sbt.hpp"
+#include "bsdf.h"
 #include "data.h"
 
 namespace pt5{ namespace material{
-
-
-__device__ float3 sample_cosine_hemisphere(float2 u){
-	u.y *= 2*M_PI;
-	float r = sqrt(u.x);
-	float z = sqrt(1-u.x);
-	return make_float3(r*cos(u.y), r*sin(u.y), z);
-}
-
-__device__ float fresnel(float ni, float eta){
-	const float g2 = eta*eta - 1 + ni;
-	if(g2 < 0) return 1;
-
-	const float g = sqrt(g2);
-	const float add = g + ni;
-	const float sub = g - ni;
-
-	const float frac = (ni*(add)-1)/(ni*sub+1);
-
-	return 0.5 * ((sub*sub)/(add*add)) * (1 + frac*frac);
-}
-
-__device__ float beckmann_g1(float vn, float alpha){
-	const float a = 1/(alpha * sqrt(1/(vn*vn) - 1));
-	return (a<1.6)?
-		(3.535*a + 2.181*a*a) / (1 + 2.276*a + 2.577*a*a)
-		: 1;
-}
 
 
 extern "C" __device__ float3 __direct_callable__diffuse_albedo(const Intersection& is){
